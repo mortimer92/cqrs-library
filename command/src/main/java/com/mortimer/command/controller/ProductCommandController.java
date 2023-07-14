f65fa24c95099dc802a653431d45bad806a66237
mortimer92 <yo_wazz_up@yahoo.com>
2023-07-14 21:19:59 +0300

    added kafka support + events + listener

--- command/src/main/java/com/mortimer/command/controller/ProductCommandController.java
+++ command/src/main/java/com/mortimer/command/controller/ProductCommandController.java
@@ -1,8 +1,10 @@
 package com.mortimer.command.controller;
 
+import com.mortimer.command.event.ProductEvent;
 import com.mortimer.command.model.Product;
 import com.mortimer.command.repository.ProductRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/products")
 public class ProductCommandController {
-    private ProductRepository repository;
+    private final ProductRepository repository;
+    private final KafkaTemplate<String, ProductEvent> kafkaTemplate;
 
-    public ProductCommandController(ProductRepository repository) {
+    public ProductCommandController(ProductRepository repository, KafkaTemplate<String, ProductEvent> kafkaTemplate) {
         this.repository = repository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody Product product){
-        repository.save(product);
+        product = repository.save(product);
+        ProductEvent event = new ProductEvent("ProductCreated", product);
+        this.kafkaTemplate.send("products", event);
         return ResponseEntity.ok().body(product);
     }
 }
--- query/src/main/java/com/mortimer/query/controller/ProductQueryController.java
+++ query/src/main/java/com/mortimer/query/controller/ProductQueryController.java
@@ -1,12 +1,18 @@
 package com.mortimer.query.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mortimer.query.event.ProductEvent;
 import com.mortimer.query.model.Product;
 import com.mortimer.query.repository.ProductRepository;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/products")
@@ -17,8 +23,28 @@ public class ProductQueryController {
         this.repository = repository;
     }
 
+    @KafkaListener(topics = "products", groupId = "products_group")
+    public void processProductEvent(String event){
+        System.out.println("Getting event "+ event);
+        ProductEvent productEvent;//Optional.empty();
+        try {
+            productEvent = new ObjectMapper().readValue(event, ProductEvent.class);
+            System.out.println(productEvent);
+
+            switch (productEvent.getType()){
+                case "ProductCreated":
+                    this.repository.save(productEvent.getProduct());
+                    break;
+                default:
+                    break;
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
     @GetMapping
     public List<Product> getProducts(){
-        return repository.findAll().stream().toList();
+        return repository.findAll().stream().collect(Collectors.toList());
     }
 }
